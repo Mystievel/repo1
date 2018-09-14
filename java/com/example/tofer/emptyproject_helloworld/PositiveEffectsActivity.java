69a4c74fc18421c28ea91ed9bcde845585e71f71
unknown <cligockia@gmail.com>
2018-09-13 20:08:11 -0700

    Not breaking radio buttons anymore when clicked, still not getting correct value though.

--- java/com/example/tofer/emptyproject_helloworld/PositiveEffectsActivity.java
+++ java/com/example/tofer/emptyproject_helloworld/PositiveEffectsActivity.java
@@ -2,15 +2,16 @@ package com.example.tofer.emptyproject_helloworld;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class PositiveEffectsActivity extends FindStrainsActivity {
 
-    //TextView relaxedLabel = (TextView)findViewById(R.id.lblRelaxed);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +22,8 @@ public class PositiveEffectsActivity extends FindStrainsActivity {
         Button cancelButton = (Button)findViewById(R.id.btnCancel); // TODO Can this be moved to global?
         Button setFilterButton = (Button)findViewById(R.id.btnSetFilter);
 
-        //final RadioGroup relaxedRadioGroup = (RadioGroup)findViewById(R.id.relaxedRadio);
+        final TextView relaxedLabel = findViewById(R.id.lblRelaxed);
+        final RadioGroup relaxedRadioGroup = findViewById(R.id.relaxedRadio);
 
         cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,11 +35,12 @@ public class PositiveEffectsActivity extends FindStrainsActivity {
         setFilterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //int checkedButton = relaxedRadioGroup.getCheckedRadioButtonId();
-                //relaxedLabel.setText(Integer.toString(checkedButton));
+                int checkedButtonID = relaxedRadioGroup.getCheckedRadioButtonId();
+                RadioButton radioButton = findViewById(checkedButtonID);
+                Toast.makeText(PositiveEffectsActivity.this,radioButton.getText(), Toast.LENGTH_SHORT).show();
 
                 // TODO uncomment the code below when the code above is fully functioning
-                startActivity(new Intent(PositiveEffectsActivity.this, FindStrainsActivity.class));
+                //startActivity(new Intent(PositiveEffectsActivity.this, FindStrainsActivity.class));
             }
         });
     }
