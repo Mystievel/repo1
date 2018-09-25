907ab8ba58a49dfa8d1a859fd43463d399866096
unknown <cligockia@gmail.com>
2018-09-24 20:32:07 -0700

    Added start of database to the results activity.

--- java/com/example/tofer/emptyproject_helloworld/ResultsActivity.java
+++ java/com/example/tofer/emptyproject_helloworld/ResultsActivity.java
@@ -28,4 +28,64 @@ public class ResultsActivity extends FindStrainsActivity {
             }
         });
     }
+
+
+    // Database for the Results
+    public class ResultsDatabase {
+        public static final String TABLE_NAME = "Results";
+
+        // Columns in the database
+        public static final String COLUMN_STRAINID = "strainID";
+        public static final String COLUMN_STRAINNAME = "strainName";
+        public static final String COLUMN_STRAINTYPE = "strainType";
+
+        // 'Column' variables
+        private int strainID;
+        private String strainName;
+        private String strainType;
+
+        // Create table SQL query
+        public static final String CREATE_TABLE =
+                "CREATE TABLE " + TABLE_NAME + "("
+                        + COLUMN_STRAINID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                        + COLUMN_STRAINNAME + " DATETIME DEFAULT CURRENT_TIMESTAMP"
+                        + COLUMN_STRAINTYPE + " TEXT,"
+                        + ")";
+
+        public ResultsDatabase() {
+        }
+
+        public ResultsDatabase(int strainID, String strainName, String strainType) {
+            this.strainID = strainID;
+            this.strainName = strainName;
+            this.strainType = strainType;
+        }
+
+        // Getters
+        public int getStrainID() {
+            return strainID;
+        }
+
+        public String getStrainName() {
+            return strainName;
+        }
+
+        public String getStrainType() {
+            return strainType;
+        }
+
+        // Setters
+        public void setStrainID(int text) {
+            this.strainID = text;
+        }
+
+        public void setStrainName(String text) {
+            this.strainName = text;
+        }
+
+        public void setStrainType(String text) {
+            this.strainType = text;
+        }
+    }
 }
+
