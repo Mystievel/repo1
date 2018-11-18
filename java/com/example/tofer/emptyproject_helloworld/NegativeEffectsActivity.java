ecb9e43956405e5d61a387477effebd769aa67aa
unknown <cligockia@gmail.com>
2018-11-18 10:19:06 -0800

    Broke, but that's ok, just need to declare the filtered array initial value shared between both positive and negative effects activities.  Pulled shared items up one level into FindStrainsActivity, and populated NEgativeEffectsACtivity.java

--- java/com/example/tofer/emptyproject_helloworld/NegativeEffectsActivity.java
+++ java/com/example/tofer/emptyproject_helloworld/NegativeEffectsActivity.java
@@ -4,10 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 
 // Todo, start populating this activity and see what shared items need to be moved.
 
 public class NegativeEffectsActivity extends FindStrainsActivity {
+    static int dryEyesBtnSelected;
+    static int dryMouthBtnSelected;
+    static int dizzyBtnSelected;
+    static int[] effectsBtnSelected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,22 +20,117 @@ public class NegativeEffectsActivity extends FindStrainsActivity {
         setContentView(R.layout.activity_negativeeffects);
 
         // Setup Button variables and listeners
-        Button cancelButton = (Button)findViewById(R.id.btnCancel);
-        Button negativeEffectsButton = (Button)findViewById(R.id.btnSetNegativeEffects);
+        Button cancelButton = findViewById(R.id.btnCancel);
+        Button setFilterButton = findViewById(R.id.btnSetNegativeEffects);
 
+        // Todo: Define all of this junk in a loop for all of the buttons and groups IDs...possible?
+        // Dry Eyes
+        final RadioButton relaxedIgnoreRadioBtn = findViewById(R.id.dryEyes_ignore);
+        final RadioButton relaxedMaxRadioBtn = findViewById(R.id.dryEyes_max);
+        final RadioButton relaxedMinRadioBtn = findViewById(R.id.dryEyes_min);
+        // Dry Mouth
+        final RadioButton happyIgnoreRadioBtn = findViewById(R.id.dryMouth_ignore);
+        final RadioButton happyMaxRadioBtn = findViewById(R.id.dryMouth_max);
+        final RadioButton happyMinRadioBtn = findViewById(R.id.dryMouth_min);
+        // Dizzy
+        final RadioButton hungryIgnoreRadioBtn = findViewById(R.id.dizzy_ignore);
+        final RadioButton hungryMaxRadioBtn = findViewById(R.id.dizzy_max);
+        final RadioButton hungryMinRadioBtn = findViewById(R.id.dizzy_min);
+
+        // Keep radio buttons selected same as the previous selection.
+        // Todo: Define all of this junk in a loop for all of the buttons and groups IDs...possible?
+        setDefaultRadioButtonSelection(dryEyesBtnSelected, relaxedMinRadioBtn, relaxedMaxRadioBtn, relaxedIgnoreRadioBtn);
+        setDefaultRadioButtonSelection(dryMouthBtnSelected, happyMinRadioBtn, happyMaxRadioBtn, happyIgnoreRadioBtn);
+        setDefaultRadioButtonSelection(dizzyBtnSelected, hungryMinRadioBtn, hungryMaxRadioBtn, hungryIgnoreRadioBtn);
+
+
+        //******************************************************************************************
+        // Event: Click Button - "Cancel"
+        //******************************************************************************************
         cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(NegativeEffectsActivity.this, FindStrainsActivity.class));
             }
-        });
+        }); //**************************************************************************************
 
-        negativeEffectsButton.setOnClickListener(new View.OnClickListener() {
+
+        //******************************************************************************************
+        // Event: Click Button - "Set Filter"
+        //******************************************************************************************
+        setFilterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Create an array for all radio buttons.
+                effectsBtnSelected = new int[]{dryEyesBtnSelected, dryMouthBtnSelected, dizzyBtnSelected};
+
+                // Loop through the buttons array and filter based on each buttons' selection.
+                for (int i = 0; i < effectsBtnSelected.length; i++) {
+                    filteredArray = filterArrayByColumn(effectsBtnSelected[i], db, effectsArray[i], filteredArray);
+                }
+
+                // Reduce the array to non-null values only.
+                // Todo fix odd bug, if you scroll down in the final list you'll see a lot of null values. not sure exactly how many.
+                // todo: most likely how the array is being initialized!!!
+                finalArraySize = getFinalArraySize(filteredArray, db);
+                //Log.d("qwrekjlhf", finalArraySize + "");
+                finalArray = new String[finalArraySize];
+                finalArray = reduceFilteredArray(filteredArray, db);
+
+                // 3) Change activity back a page to the FindStrainsActivity.
                 startActivity(new Intent(NegativeEffectsActivity.this, FindStrainsActivity.class));
+            }
+        }); //**************************************************************************************
+
+        // 'Dry Eyes' Buttons
+        relaxedIgnoreRadioBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { dryEyesBtnSelected = IGNORE;
+            }
+        });
+        relaxedMinRadioBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { dryEyesBtnSelected = MIN;
+            }
+        });
+        relaxedMaxRadioBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { dryEyesBtnSelected = MAX;
             }
         });
 
+        // 'Dry Mouth' Buttons
+        happyIgnoreRadioBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { dryMouthBtnSelected = IGNORE;
+            }
+        });
+        happyMinRadioBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { dryMouthBtnSelected = MIN;
+            }
+        });
+        happyMaxRadioBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { dryMouthBtnSelected = MAX;
+            }
+        });
+
+        // 'Dizzy' Buttons
+        hungryIgnoreRadioBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { dizzyBtnSelected = IGNORE;
+            }
+        });
+        hungryMinRadioBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { dizzyBtnSelected = MIN;
+            }
+        });
+        hungryMaxRadioBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { dizzyBtnSelected = MAX;
+            }
+        });
     }
 }
--- java/com/example/tofer/emptyproject_helloworld/PositiveEffectsActivity.java
+++ java/com/example/tofer/emptyproject_helloworld/PositiveEffectsActivity.java
@@ -8,18 +8,11 @@ import android.widget.Button;
 import android.widget.RadioButton;
 
 public class PositiveEffectsActivity extends FindStrainsActivity {
-    // Defines
-    int IGNORE = 0;
-    int MIN = 1;
-    int MAX = 2;
-    String BLANK_ENTRY = "";
-
     // Local variables
     static int relaxedBtnSelected;
     static int happyBtnSelected;
     static int hungryBtnSelected;
     static int[] effectsBtnSelected;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,22 +43,23 @@ public class PositiveEffectsActivity extends FindStrainsActivity {
         setDefaultRadioButtonSelection(happyBtnSelected, happyMinRadioBtn, happyMaxRadioBtn, happyIgnoreRadioBtn);
         setDefaultRadioButtonSelection(hungryBtnSelected, hungryMinRadioBtn, hungryMaxRadioBtn, hungryIgnoreRadioBtn);
 
+        //******************************************************************************************
+        // Event: Click Button - "Cancel"
+        //******************************************************************************************
         cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(PositiveEffectsActivity.this, FindStrainsActivity.class));
             }
-        });
+        }); //**************************************************************************************
 
-        //**********************************************************************************************
+
+        //******************************************************************************************
         // Event: Click Button - "Set Filter"
-        //**********************************************************************************************
+        //******************************************************************************************
         setFilterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Start out with a list of all strains.
-                String[] filteredArray = getAllStrainNames();
-
                 // Create an array for all radio buttons.
 				effectsBtnSelected = new int[]{relaxedBtnSelected, happyBtnSelected, hungryBtnSelected};
 
@@ -76,6 +70,7 @@ public class PositiveEffectsActivity extends FindStrainsActivity {
 
                 // Reduce the array to non-null values only.
                 // Todo fix odd bug, if you scroll down in the final list you'll see a lot of null values. not sure exactly how many.
+                // todo: most likely how the array is being initialized!!!
                 finalArraySize = getFinalArraySize(filteredArray, db);
 				//Log.d("asdkljsdglkj", finalArraySize + "");
                 finalArray = new String[finalArraySize];
@@ -84,55 +79,37 @@ public class PositiveEffectsActivity extends FindStrainsActivity {
                 // 3) Change activity back a page to the FindStrainsActivity.
                 startActivity(new Intent(PositiveEffectsActivity.this, FindStrainsActivity.class));
             }
-        });
-
+        }); //**************************************************************************************
 
         // 'Relaxed' Buttons
         relaxedIgnoreRadioBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                relaxedBtnSelected = IGNORE;
-                //listItem1 = db.getStrainData(3).getStrainName();
-                //long id = db.addStrain(new CannabisStrainDatabase_Definition("Cannabis Strain 2", 55.5));
-                //Toast.makeText(PositiveEffectsActivity.this,String.valueOf(id), Toast.LENGTH_SHORT).show();
-                //Toast.makeText(PositiveEffectsActivity.this,String.valueOf(relaxedRadioGroup.getCheckedRadioButtonId()), Toast.LENGTH_SHORT).show();
+            public void onClick(View view) { relaxedBtnSelected = IGNORE;
             }
         });
-
         relaxedMinRadioBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                relaxedBtnSelected = MIN;
-                //Toast.makeText(PositiveEffectsActivity.this,String.valueOf(db.getStrainData(3).getStrainName()), Toast.LENGTH_SHORT).show();
-                //Toast.makeText(PositiveEffectsActivity.this,String.valueOf(relaxedRadioGroup.getCheckedRadioButtonId()), Toast.LENGTH_SHORT).show();
+            public void onClick(View view) { relaxedBtnSelected = MIN;
             }
         });
-
         relaxedMaxRadioBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                relaxedBtnSelected = MAX;
-                //long dbRows = db.getStrainDatabaseRows();
-                //Toast.makeText(PositiveEffectsActivity.this, String.valueOf(dbRows), Toast.LENGTH_SHORT).show();
-                //Toast.makeText(PositiveEffectsActivity.this,String.valueOf(relaxedRadioGroup.getCheckedRadioButtonId()), Toast.LENGTH_SHORT).show();
+            public void onClick(View view) { relaxedBtnSelected = MAX;
             }
         });
 
         // 'Happy' Buttons
         happyIgnoreRadioBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                happyBtnSelected = IGNORE;
+            public void onClick(View view) { happyBtnSelected = IGNORE;
             }
         });
-
         happyMinRadioBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 happyBtnSelected = MIN;
             }
         });
-
         happyMaxRadioBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -140,146 +117,21 @@ public class PositiveEffectsActivity extends FindStrainsActivity {
             }
         });
 
-
         // 'Hungry' Buttons
         hungryIgnoreRadioBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                hungryBtnSelected = IGNORE;
+            public void onClick(View view) { hungryBtnSelected = IGNORE;
             }
         });
-
         hungryMinRadioBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                hungryBtnSelected = MIN;
+            public void onClick(View view) { hungryBtnSelected = MIN;
             }
         });
-
         hungryMaxRadioBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                hungryBtnSelected = MAX;
+            public void onClick(View view) { hungryBtnSelected = MAX;
             }
         });
-    } //********************************************************************************************
-
-
-    //**********************************************************************************************
-    //                    Gets any single Column from the Strains Database
-    //**********************************************************************************************
-    public String[] getAllStrainNames() {
-        String[] allStrainNames = new String[(int) db.getStrainDatabaseRows()];
-        for (int i = 0; i < db.getStrainDatabaseRows(); i++) {
-            allStrainNames[i] = db.getStrainData(i+1).getStrainName();
-        }
-        return allStrainNames;
-    }
-
-
-    //**********************************************************************************************
-    //                              Set Default Radio Button Selection
-    //**********************************************************************************************
-    public void setDefaultRadioButtonSelection(int btnSelection, RadioButton minBtn, RadioButton maxBtn, RadioButton ignoreBtn) {
-        if (btnSelection == MIN) {
-            minBtn.setChecked(true);
-        } else if (btnSelection == MAX) {
-            maxBtn.setChecked(true);
-        } else {
-            ignoreBtn.setChecked(true);
-        }
-    } //********************************************************************************************
-
-
-    //**********************************************************************************************
-    //                             Filter Array by db Column
-    //
-    // Search a single column within a database, make invalid entries == "" (null), and return the final array.
-    // How to call: filteredArray = reduceDBArrayByColumnSearch(relaxedBtnSelected);
-    // Later we take out all "" (null) entries, so we know the array length = database entries.
-    //
-    // Todo: Update "0.5" to something else (a percentile based off search intensity bar).
-    // todo find the 'null' list item bug. is it in this method?
-    //**********************************************************************************************
-    public String[] filterArrayByColumn(int btnResult, CannabisStrainDatabase_Helper db, int effect, String[] originalArray) {
-        // Declare local variables
-        int databaseRows = (int) db.getStrainDatabaseRows();
-        int index;
-        String newArray[] = new String[databaseRows];
-
-        if (btnResult == MIN) {
-            for (index = 0; index < databaseRows; index++) {
-                if (db.getStrainData(index + 1).getEffect(effect) < 0.5) {
-                	if (originalArray[index] == BLANK_ENTRY) {
-                        newArray[index] = BLANK_ENTRY;
-                    } else {
-                        newArray[index] = db.getStrainData(index + 1).getStrainName();
-                    }
-                } else {
-                    newArray[index] = BLANK_ENTRY;
-                }
-            }
-        } else if (btnResult == MAX) {
-            for (index = 0; index < databaseRows; index++) {
-                if (db.getStrainData(index + 1).getEffect(effect) >= 0.5) {
-                    if (originalArray[index] == BLANK_ENTRY) {
-                        newArray[index] = BLANK_ENTRY;
-                    } else {
-                        newArray[index] = db.getStrainData(index + 1).getStrainName();
-                    }
-                } else {
-                    newArray[index] = BLANK_ENTRY;
-                }
-            }
-        } else {
-            for (index = 0; index < databaseRows; index++) {
-                newArray[index] = db.getStrainData(index + 1).getStrainName();
-            }
-        }
-
-        // Return the reduced array
-        return newArray;
-    } //********************************************************************************************
-
-
-    //**********************************************************************************************
-    //                             Get Filtered Array Size
-    //**********************************************************************************************
-    public int getFinalArraySize(String[] filteredArray, CannabisStrainDatabase_Helper db) {
-        // Declare local variables
-        int databaseRows = (int) db.getStrainDatabaseRows();
-        int index;
-        int count = 0;
-
-        // get the size that the new array will be
-        for (index = 0; index < databaseRows; index++) {
-            if (filteredArray[index] != BLANK_ENTRY) {
-                count++;
-            }
-        }
-        return count;
-    } //********************************************************************************************
-
-
-    //**********************************************************************************************
-    //                             Reduce Filtered Array
-    //**********************************************************************************************
-    public String[] reduceFilteredArray(String[] filteredArray, CannabisStrainDatabase_Helper db) {
-        // Declare local variables
-        int databaseRows = (int) db.getStrainDatabaseRows();
-        int index;
-        int count = getFinalArraySize(filteredArray, db);
-        int subtractor = 0;
-        String reducedArray[] = new String[databaseRows];
-
-        // Now populate the reducedArray without the blank items from the original array.
-        for (index = 0; index < count; index++) {
-            if (filteredArray[index] != BLANK_ENTRY) {
-                reducedArray[index - subtractor] = filteredArray[index];
-            } else {
-                subtractor++;
-            }
-        }
-        return reducedArray;
     } //********************************************************************************************
 }
