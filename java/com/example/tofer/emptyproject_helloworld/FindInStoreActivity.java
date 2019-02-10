f460f35d5876495316f5f2396ec2d42410c5a566
unknown <cligockia@gmail.com>
2019-02-10 15:44:43 -0800

    Still trying to show multiple stores upon loading the map.

--- java/com/example/tofer/emptyproject_helloworld/FindInStoreActivity.java
+++ java/com/example/tofer/emptyproject_helloworld/FindInStoreActivity.java
@@ -8,7 +8,10 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.CardView;
 import android.util.Log;
@@ -17,7 +20,12 @@ import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -42,11 +50,20 @@ import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
 import java.util.Arrays;
 import java.util.List;
 
-// todo: medium priority - update map data/location every 0.000xxx degrees of lat/lng.
 
-public class FindInStoreActivity extends MainActivity implements OnMapReadyCallback {
+public class FindInStoreActivity extends MainActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+	// Defines
+	double INCREMENTAL = 0.1;
+	double PROX_RADIUS = 1;
+
 	// Globals
 	GoogleMap mMap;
+	private GoogleApiClient client;
+	private LocationRequest locationRequest;
+	public static final int REQUEST_LOCATION_CODE = 99;
+	int PROXIMITY_RADIUS = 10000;
+	Object dataTransfer[] = new Object[2];
+	GetNearbyPlacesData getNearbyPlacesData = new GetNearbyPlacesData();
 	AutocompleteSupportFragment placeAutoComplete;
 
 	//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -60,8 +77,6 @@ public class FindInStoreActivity extends MainActivity implements OnMapReadyCallb
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.find_in_store_activity);
 
-		// todo: search not working, need to encompass within fragment???
-		// todo: see link:   https://stackoverflow.com/questions/38174075/place-autocomplete-in-mapfragment-invisible-on-old-apis
 		if (!Places.isInitialized()) {
 			Places.initialize(getApplicationContext(), "AIzaSyDPSfWIKGi6Swv00Y-JTKW6-NTSnkeatCQ");
 		}
@@ -69,7 +84,7 @@ public class FindInStoreActivity extends MainActivity implements OnMapReadyCallb
 		// Useful Link: https://developers.google.com/places/android-sdk/autocomplete#add_an_autocomplete_widget.
 		placeAutoComplete = (AutocompleteSupportFragment) getSupportFragmentManager().findFragmentById(R.id.autoCompleteSearch);
 		// Specify the types of place data to return.
-		placeAutoComplete.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS, Place.Field.PHONE_NUMBER, Place.Field.LAT_LNG));
+		placeAutoComplete.setPlaceFields(Arrays.asList(Place.Field.NAME, Place.Field.ADDRESS, Place.Field.PHONE_NUMBER, Place.Field.LAT_LNG));
 		// Auto-complete listener.
 		placeAutoComplete.setOnPlaceSelectedListener(new PlaceSelectionListener() {
 			@Override
@@ -77,19 +92,16 @@ public class FindInStoreActivity extends MainActivity implements OnMapReadyCallb
 				double latitude = place.getLatLng().latitude;
 				double longitude = place.getLatLng().longitude;
 				String placeName = place.getName();
-				Log.d("weredty", "Name: " + placeName);
-				Log.d("weredty", "Lat: " + latitude);
-				Log.d("weredty", "Lng: " + longitude);
+				String placeTag = place.getName() + "\n" + place.getPhoneNumber() + "\n" + Place.Field.ADDRESS;
+				//Log.d("MapsSelPl", "Name: " + placeName + ". Lat: " + latitude + ". Lng: " + longitude);
 
 				mMap.clear();
-				// todo: https://www.google.com/search?q=android+add+marker+from+autocomplete+search&source=lnt&tbs=qdr:y&sa=X&ved=0ahUKEwiQ0buMl6vgAhVSR60KHeSWDikQpwUIJg&biw=1920&bih=969
-				moveMapToLocationWithMarker(mMap, latitude, longitude, placeName);
-				Log.d("MapsSelPl", "Place selected: " + place.getName() + ", " + place.getId());
+				moveMapToLocationWithMarker(mMap, latitude, longitude, placeTag);
 			}
 
 			@Override
 			public void onError(Status status) {
-				// todo: handle the error
+				Toast.makeText(FindInStoreActivity.this, "Error getting data.", Toast.LENGTH_SHORT).show();
 				Log.d("MapsSelPl", "An error occurred: " + status);
 			}
 		});
@@ -144,20 +156,26 @@ public class FindInStoreActivity extends MainActivity implements OnMapReadyCallb
 		}); //**************************************************************************************
 	}
 
+
 	@Override
 	public void onMapReady(GoogleMap googleMap) {
+		// Save instance to the map object.
 		mMap = googleMap;
-		Location location = getDevicesLastKnownLocation(googleMap);
-		handleNullLocation(googleMap, location);
 
-		// now get nearest 5 dispenseries.
+		// Get the user's most accurate, last known location.
+		Location location = getDevicesLastKnownLocation(googleMap);
+
+		// Show nearby dispensaries on the map.
+		String url = getUrl(location.getLatitude(), location.getLongitude(), "hospital");
+		dataTransfer[0] = mMap;
+		dataTransfer[1] = url;
+		getNearbyPlacesData.execute(dataTransfer);
 	}
 
 
 	public void moveMapToLocationWithMarker(GoogleMap googleMap, double latitude, double longitude, String tag) {
-		double inc = 0.1;
 		LatLng userLocation = new LatLng(latitude, longitude);
-		LatLngBounds mapBounds = new LatLngBounds(new LatLng(latitude - inc, longitude - inc), new LatLng(latitude + inc, longitude + inc));
+		LatLngBounds mapBounds = new LatLngBounds(new LatLng(latitude - INCREMENTAL, longitude - INCREMENTAL), new LatLng(latitude + INCREMENTAL, longitude + INCREMENTAL));
 		googleMap.addMarker(new MarkerOptions().position(userLocation).title(tag));
 		googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(mapBounds, 0));
 	}
@@ -165,13 +183,13 @@ public class FindInStoreActivity extends MainActivity implements OnMapReadyCallb
 
 	public void handleNullLocation(GoogleMap googleMap, Location location) {
 		if (location != null) {
-			Log.d("nulloc", "non-null location. valid.");
+			//Log.d("nulloc", "non-null location. valid.");
 			// Get the users current location.
 			double longitude = location.getLongitude();
 			double latitude = location.getLatitude();
-			moveMapToLocationWithMarker(googleMap, latitude, longitude,"Store x");
+			moveMapToLocationWithMarker(googleMap, latitude, longitude,"you");
 		} else {
-			Log.d("nulloc", "null location. invalid.");
+			//Log.d("nulloc", "null location. invalid.");
 			moveMapToLocationWithMarker(googleMap,37.399976,-122.058106, "Google Campus");
 		}
 	}
@@ -188,13 +206,13 @@ public class FindInStoreActivity extends MainActivity implements OnMapReadyCallb
 				googleMap.getUiSettings().setMyLocationButtonEnabled(true);
 
 				Location location = mLocationManager.getLastKnownLocation(provider);
-				Log.d("LastDeviceLocation", String.format("Provider: %s. Location: %s", provider, location));
+				//Log.d("LastDeviceLocation", String.format("Provider: %s. Location: %s", provider, location));
 
 				if (location == null) {
 					continue; // Advance to next loop iteration.
 				}
 				if (bestLocation == null || location.getAccuracy() < bestLocation.getAccuracy()) {
-					Log.d("LastDeviceLocation", String.format("Best last known location so far: %s", location));
+					//Log.d("LastDeviceLocation", String.format("Best last known location so far: %s", location));
 					bestLocation = location;
 				}
 			} else {
@@ -202,7 +220,65 @@ public class FindInStoreActivity extends MainActivity implements OnMapReadyCallb
 				ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
 			}
 		}
-		Log.d("LastDeviceLocation", String.format("Best location: %s", bestLocation));
+		//Log.d("LastDeviceLocation", String.format("Best location: %s", bestLocation));
+
+		handleNullLocation(googleMap, bestLocation);
 		return bestLocation;
+	}
+
+
+	private String getUrl(double latitude , double longitude , String nearbyPlace) {
+		StringBuilder googlePlaceUrl = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+		googlePlaceUrl.append("location=" + latitude + "," + longitude);
+		googlePlaceUrl.append("&radius=" + PROXIMITY_RADIUS);
+		googlePlaceUrl.append("&type=" + nearbyPlace);
+		googlePlaceUrl.append("&sensor=true");
+		googlePlaceUrl.append("&key=" + "AIzaSyDPSfWIKGi6Swv00Y-JTKW6-NTSnkeatCQ");
+		Log.d("MapsActivity", "url = " + googlePlaceUrl.toString());
+		return googlePlaceUrl.toString();
+	}
+
+
+	@Override
+	public void onConnected(@Nullable Bundle bundle) {
+		locationRequest = new LocationRequest();
+		locationRequest.setInterval(100);
+		locationRequest.setFastestInterval(1000);
+		locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+		if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION ) == PackageManager.PERMISSION_GRANTED) {
+			LocationServices.FusedLocationApi.requestLocationUpdates(client, locationRequest, this);
+		}
+	}
+
+
+	public boolean checkLocationPermission() {
+		if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)  != PackageManager.PERMISSION_GRANTED ) {
+			if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)) {
+				ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.ACCESS_FINE_LOCATION },REQUEST_LOCATION_CODE);
+			}
+			else {
+				ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.ACCESS_FINE_LOCATION },REQUEST_LOCATION_CODE);
+			}
+			return false;
+
+		} else {
+			return true;
+		}
+	}
+
+
+	@Override
+	public void onConnectionSuspended(int i) {
+	}
+
+
+	@Override
+	public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+	}
+
+
+	@Override
+	public void onLocationChanged(Location location) {
 	}
 }
